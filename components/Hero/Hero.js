553889b075b2ba63658690998b5c9aac843c76f8
Denis Pankov <idenispankov@gmail.com>
2021-08-13 22:45:25 -0700

    Hero section ready - fully responsive

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,22 +1,32 @@
 import styles from './Hero.module.css';
-import MainStyles from '../../styles/MainStyles.module.css';
-// import Image from 'next/image';
-// import hero from '../../public/images/hero.png';
+// import MainStyles from '../../styles/MainStyles.module.css';
+import Image from 'next/image';
+import hero from '../../public/images/hero_large.png';
 
 export default function Hero() {
   return (
     <section className={styles.section}>
-      <h1>HELLO</h1>
-      {/* <div className={styles.text__container}>
+      <div className={styles.text__container}>
         <h1 className={styles.section__heading}>
-          Your task. Done by data and web development students.{' '}
+          Your task.
+          <br className={styles.break_line} /> Done by data and web development
+          students. <br className={styles.break_line} />
           <span className={styles.section__heading_span}>For free!</span>
         </h1>
-        <button className={MainStyles.button_type_dark}>Delegate a task</button>
+        <button className={`${styles.button}`}>Delegate a task</button>
       </div>
       <div className={styles.section__image}>
-        <Image src={hero} alt='girl meditating' layout='fixed' />
-      </div> */}
+        <Image
+          src={hero}
+          alt='girl meditating'
+          layout='responsive'
+          width={320}
+          height={313}
+        />
+        <button className={`${styles.button} ${styles.button_type_mobile}`}>
+          Delegate a task
+        </button>
+      </div>
     </section>
   );
 }
