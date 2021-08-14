7dc06f1e2610a1bd9106537dbc0a189c37a38497
Denis Pankov <idenispankov@gmail.com>
2021-08-13 18:31:28 -0700

    mobile menu redy Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,46 +2,105 @@ import styles from './Navbar.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../../public/images/logo.svg';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <nav className={styles.navbar}>
         <Link href='/'>
-          <a className={styles.logo__container}>
-            <div className={styles.logo__image}>
-              <Image src={logo} alt='Practicum by Yandex logo' />
-            </div>
+          <a className={styles.navbar__logo_image}>
+            <Image src={logo} alt='Practicum by Yandex logo' />
           </a>
         </Link>
-        <ul className={styles.list}>
-          <li className={styles.list__item}>
+        <ul className={styles.navbar__list}>
+          <li className={styles.navbar__list_item}>
             <Link href='#we-can-help'>
-              <a className={styles.list__item_link}>We can help!</a>
+              <a className={styles.navbar__list_item_link}>We can help!</a>
             </Link>
           </li>
-          <li className={styles.list__item}>
+          <li className={styles.navbar__list_item}>
             <Link href='#about'>
-              <a className={styles.list__item_link}>About</a>
+              <a className={styles.navbar__list_item_link}>About</a>
             </Link>
           </li>
-          <li className={styles.list__item}>
+          <li className={styles.navbar__list_item}>
             <Link href='#how-to-start'>
-              <a className={styles.list__item_link}>How to start</a>
+              <a className={styles.navbar__list_item_link}>How to start</a>
             </Link>
           </li>
-          <li className={styles.list__item}>
+          <li className={styles.navbar__list_item}>
             <Link href='#projects'>
-              <a className={styles.list__item_link}>Projects</a>
+              <a className={styles.navbar__list_item_link}>Projects</a>
             </Link>
           </li>
-          <li className={styles.list__item}>
+          <li className={styles.navbar__list_item}>
             <Link href='#contacts'>
-              <a className={styles.list__item_link}>Contacts</a>
+              <a className={styles.navbar__list_item_link}>Contacts</a>
             </Link>
           </li>
         </ul>
-        <button className={styles.button}>Delegate a task</button>
+        <button className={styles.navbar__button}>Delegate a task</button>
+
+        <div
+          className={`${styles.hamburger} ${isOpen && styles.open}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className={styles.hamburger_before}></div>
+          <div className={styles.hamburger_after}></div>
+        </div>
+
+        <ul className={`${styles.hamburger__list} ${isOpen && styles.show}`}>
+          <li
+            className={styles.navbar__list_item}
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href='#we-can-help'>
+              <a className={styles.navbar__list_item_link}>We can help!</a>
+            </Link>
+          </li>
+          <li
+            className={styles.navbar__list_item}
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href='#about'>
+              <a className={styles.navbar__list_item_link}>About</a>
+            </Link>
+          </li>
+          <li
+            className={styles.navbar__list_item}
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href='#how-to-start'>
+              <a className={styles.navbar__list_item_link}>How to start</a>
+            </Link>
+          </li>
+          <li
+            className={styles.navbar__list_item}
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href='#projects'>
+              <a className={styles.navbar__list_item_link}>Projects</a>
+            </Link>
+          </li>
+          <li
+            className={styles.navbar__list_item}
+            onClick={() => setIsOpen(false)}
+          >
+            <Link href='#contacts'>
+              <a className={styles.navbar__list_item_link}>Contacts</a>
+            </Link>
+          </li>
+          <li>
+            <button
+              className={`${styles.navbar__button} ${styles.navbar__button_mobile}`}
+            >
+              Delegate a task
+            </button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
