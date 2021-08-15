5c0bd8fcae8ffc5d58a69c17ad96c8a1052a0dd5
Denis Pankov <idenispankov@gmail.com>
2021-08-15 04:41:28 -0700

    delete hero, started navbar desktop, global css vars created

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,112 +1,43 @@
 import styles from './Navbar.module.css';
 import Link from 'next/link';
-import Image from 'next/image';
-import logo from '../../public/images/logo.svg';
-import { useState } from 'react';
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
-    <header className={styles.header}>
+    <header>
       <nav className={styles.navbar}>
         <Link href='/'>
-          <a
-            className={styles.navbar__logo_image}
-            onClick={() => setIsOpen(false)}
-          >
-            <Image src={logo} alt='Practicum by Yandex logo' />
+          <a>
+            <div className={styles.logo}></div>
           </a>
         </Link>
-
-        {/* Mobile */}
-        <div
-          className={`${styles.hamburger} ${isOpen && styles.open}`}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <div className={styles.hamburger_before}></div>
-          <div className={styles.hamburger_after}></div>
-        </div>
-
-        <ul className={`${styles.hamburger__list} ${isOpen && styles.show}`}>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='#we-can-help'>
-              <a className={styles.navbar__list_item_link}>We can help!</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='#about'>
-              <a className={styles.navbar__list_item_link}>About</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='#how-to-start'>
-              <a className={styles.navbar__list_item_link}>How to start</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='#projects'>
-              <a className={styles.navbar__list_item_link}>Projects</a>
-            </Link>
-          </li>
-          <li
-            className={styles.navbar__list_item}
-            onClick={() => setIsOpen(false)}
-          >
-            <Link href='#contacts'>
-              <a className={styles.navbar__list_item_link}>Contacts</a>
+        <ul className={styles.list}>
+          <li>
+            <Link href='we-can-help'>
+              <a className={styles.list__item}>We can help!</a>
             </Link>
           </li>
           <li>
-            <button
-              className={`${styles.navbar__button} ${styles.navbar__button_mobile}`}
-            >
-              Delegate a task
-            </button>
-          </li>
-        </ul>
-
-        {/* Desktop */}
-        <ul className={styles.navbar__list}>
-          <li className={styles.navbar__list_item}>
-            <Link href='#we-can-help'>
-              <a className={styles.navbar__list_item_link}>We can help!</a>
+            <Link href='about'>
+              <a className={styles.list__item}>About</a>
             </Link>
           </li>
-          <li className={styles.navbar__list_item}>
-            <Link href='#about'>
-              <a className={styles.navbar__list_item_link}>About</a>
+          <li>
+            <Link href='we-can-help'>
+              <a className={styles.list__item}>How to start</a>
             </Link>
           </li>
-          <li className={styles.navbar__list_item}>
-            <Link href='#how-to-start'>
-              <a className={styles.navbar__list_item_link}>How to start</a>
+          <li>
+            <Link href='we-can-help'>
+              <a className={styles.list__item}>Projects</a>
             </Link>
           </li>
-          <li className={styles.navbar__list_item}>
-            <Link href='#projects'>
-              <a className={styles.navbar__list_item_link}>Projects</a>
-            </Link>
-          </li>
-          <li className={styles.navbar__list_item}>
-            <Link href='#contacts'>
-              <a className={styles.navbar__list_item_link}>Contacts</a>
+          <li>
+            <Link href='we-can-help'>
+              <a className={styles.list__item}>Contacts</a>
             </Link>
           </li>
         </ul>
-        <button className={styles.navbar__button}>Delegate a task</button>
+        <button className={styles.button}>Delegate a task</button>
       </nav>
     </header>
   );
